cf511e86aa1ab07bd63a58ea55cedbf9458e8e99
nairens.world <nairens.world@gmail.com>
2025-10-17 11:43:00 +0300

    Add shadcn components

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { Button } from './components/ui/button';
 
 function App() {
   const [message, setMessage] = useState('');
@@ -11,8 +12,12 @@ function App() {
       .catch(error => console.error('Error fetching message:', error));
   }, []);
 
-  return <p className='font-bold 
-  text-3xl bg-amber-800 stroke-amber-50'>{message}</p>
+  return (
+    <div className='p-4'>
+    <p className='font-bold text-3xl'>{message}</p>
+    <Button>Click Me</Button>
+    </div>
+  )
 }
 
 export default App
