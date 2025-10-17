506b856f183e1344d45f5fefd13171c7bb86e0b1
nairens.world <nairens.world@gmail.com>
2025-10-17 13:14:03 +0300

    Add API request validataions with zod

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { GoogleGenAI } from '@google/genai';
+import z from 'zod';
 
 dotenv.config();
+const client = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
@@ -14,6 +18,29 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/api/hello', (req: Request, res: Response) => {
    res.send({ message: 'Hi Itay, welcome to the server client world' });
+});
+
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'prompt cannot be empty')
+      .max(1000, 'prompt is too long'),
+});
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+   const parsedResult = chatSchema.safeParse(req.body);
+
+   if (!parsedResult.success) {
+      return res.status(400).json({ error: parsedResult.error.format() });
+   }
+
+   const { prompt } = req.body;
+   const response = await client.models.generateContent({
+      model: 'gemini-2.5-flash',
+      contents: prompt,
+   });
+   res.json({ message: response.text });
 });
 
 app.listen(port, () => {
