de5b75fce2a51932763c25f64df06a8a4143e4e1
eitan.arat <eitan.arat@gmail.com>
2025-10-17 10:09:53 +0300

    Start client and server concurrently

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -11,6 +11,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send(process.env.GEMINI_API_KEY || "No API Key Set");
 });
 
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.send({ message: "Hello, World!" });
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
